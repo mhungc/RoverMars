c201c5cbf80a11434615e14f49e2c8e79e48700d
Marco Hung <mhungc@gmail.com>
2019-10-20 14:28:51 +0200

    Added position class for Rover

---
+++ RoverMars.Rover.Domain/Position.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RoverMars.Rover.Domain
+{
+    public class Position
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public Position()
+        {
+            X = 0;
+            Y = 0;
+        }
+    }
+}
