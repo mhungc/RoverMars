04f6908586487b998066a201c98f191fbb7cecb4
Marco Hung <mhungc@gmail.com>
2019-10-20 14:40:40 +0200

    Apply strategy pattern for RoverSpaceVehicle

--- RoverMars.Rover.Domain/RoverSpaceVehicle.cs
+++ RoverMars.Rover.Domain/RoverSpaceVehicle.cs
@@ -1,4 +1,5 @@
 ﻿using RoverMars.Rover.Domain.Abstract;
+using RoverMars.Rover.Domain.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,5 +8,22 @@ namespace RoverMars.Rover.Domain
 {
     public class RoverSpaceVehicle : SpaceVehicleEngine
     {
+        public RoverSpaceVehicle(
+                   ITurnLeft turnLeftAlgorithm
+                    , ITurnRight turnRightAlgorithm
+                    , IAdvance advanceAlgorithm
+                    , MarsOrientations roverOrientation)
+        {
+            _turnLeftBehavior = turnLeftAlgorithm;
+            _turnRightBehavior = turnRightAlgorithm;
+            _advanceBehavior = advanceAlgorithm;
+            _roverPositionInMars = new Position(); //Default 0 , 0 Coordination;
+            _roverOrientationsInMars = roverOrientation;
+        }
+
+        public override void ProcessCommand(char command)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
