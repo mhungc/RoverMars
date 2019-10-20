00d2fccc437c4dccd63fcd2863aeabd16d0e2b9e
Marco Hung <mhungc@gmail.com>
2019-10-20 14:31:29 +0200

    Added class that define rovert and a enum orientation in mars

---
+++ RoverMars.Rover.Domain/Orientations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RoverMars.Rover.Domain
+{
+    public enum MarsOrientations
+    {
+        N = 1,
+        E = 2,
+        S = 3,
+        W = 4
+    }
+}
