fff5e909691eade2f22ef1549ee2e8ec82aa1583
Marco Hung <mhungc@gmail.com>
2019-10-20 14:12:05 +0200

    Initial

---
+++ RoverMars/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RoverMars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
