6678618edb22a8dd70d93c13854741f1a66ff911
Marco Hung <mhungc@gmail.com>
2019-10-20 14:33:45 +0200

    Heritage in Rover from SpaceVehicleEngine

--- RoverMars.Rover.Domain/RoverSpaceVehicle.cs
+++ RoverMars.Rover.Domain/RoverSpaceVehicle.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using RoverMars.Rover.Domain.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace RoverMars.Rover.Domain
 {
-    public class RoverSpaceVehicle
+    public class RoverSpaceVehicle : SpaceVehicleEngine
     {
     }
 }
